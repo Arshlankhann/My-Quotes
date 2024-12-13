9147c593e5a8baec5ea1164945d58ffff4bb717a
GitShailendra <kumarshailrndra7355@gmail.com>
2024-12-13 16:40:15 +0530

    fix:code push

--- models/post.js
+++ models/post.js
@@ -3,14 +3,10 @@ const mongoose = require("mongoose")
 mongoose.connect('mongodb://127.0.0.1:27017/My_Quotes')  //new
 
 const postSchema = mongoose.Schema({
-    username: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:"post"
-        
-    },
+   
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "post"
+        ref: "user"
     },
     profilepic: {
         type: String,
